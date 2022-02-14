f177f57ba5c0ab5fea8dc2bae56e4b7987de50cb
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-02-14 18:28:24 +0100

    update 14_02_2022

--- Python/projects/1-le_juste_prix.py
+++ Python/projects/1-le_juste_prix.py
@@ -3,10 +3,11 @@
 price = 21550
 price = int(price)
 modèle = "Ford Fiesta 2020"
+score = 100
 
 
-user_id = "Anthony"
-user_pass = "hello"
+user_id = "root"
+user_pass = "root"
 online = False
 
 print("Bonjour, veuillez vous connecter pour jouer !\n")
@@ -20,12 +21,18 @@ if username == user_id and password == user_pass:
     print("Vous allez maintenant jouer au 'Juste Prix' !\n Vous allez devoir deviner le prix d'une voiture, voici le modèle:", modèle)
     winner = False
     while winner == False:
+        if score == 0:
+            print("Dommage, vous avez perdu ! Au revoir.")
+            break
         number = input("Quelle est votre estimation? ")
         number = int(number)
         if number == price:
             print("Félicitations vous avez gagné !\n")
+            winner == True
+            print("Vous avez obtenu un score de", score)
             other_game = input("Voulez-vous recommencer? Tapez Oui ou Non\n")
             if other_game == "Oui":
+                score = 100
                 continue
             elif other_game == "Non":
                 print("Au revoir!")
@@ -34,10 +41,12 @@ if username == user_id and password == user_pass:
                 print("Erreur: Vous devez répondre par 'Oui' ou 'Non' !")
                 break
         elif number > price and number < 1000000:
-            print("C'est moins!")
+            score = score - 10
+            print("C'est moins! Score: ", score)
             continue
         elif number < price and number > 1:
-            print("C'est plus!")
+            score = score - 10
+            print("C'est plus!  Score: ", score)
             continue
         else:
             print("Erreur. Veuillez entrer des valeurs correctes !")
