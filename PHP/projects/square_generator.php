7fb810b8ba27e7a162da8d2ab92dac8e91144622
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-02-15 23:20:39 +0100

    update 15_02_2022

---
+++ PHP/projects/square_generator.php
@@ -0,0 +1,37 @@
+<?php
+
+$a = 50;
+$b = 20;
+
+check_valid_values($a, $b);
+
+function check_valid_values($a, $b){
+    if($a > 0 & $b > 0){
+        print_square_body($a, $b);
+    }    
+    else{
+        print "Les valeurs ne sont pas correctes!";
+    }
+}
+
+function print_square_body($a, $b){
+    for($j = 1; $j <= $b; $j++){
+        for($i = 1; $i <= $a; $i++){
+            print "0";
+        }
+        print "<br>";
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<style>
+		body {
+			background-color: #000000;
+			color: #fff;
+            text-align: center;
+		}
+	</style>
