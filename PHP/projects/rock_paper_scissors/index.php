6f402c70305c61f9fb32aea15fd027b240186bb0
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-03-07 22:14:20 +0100

    update 07_03_2022

---
+++ PHP/projects/rock_paper_scissors/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Rock Paper Scissors Game</title>
+</head>
+<body>
+    <header>
+        <div class="top_section">
+            <img src="./img/title.png" alt="">
+        </div>
+    </header>
+    <section class="inside">
+        <section class="content">
+            <a href="#">
+                <div class="play-button">
+                    <h1>PLAY NOW </h1>
+                </div>
+            </a>
+        </section>
+    </section>
+
+</body>
+</html>
