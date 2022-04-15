ea597e79cfe90819973d93f5f792641ee7299805
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-04-15 11:57:36 +0200

    update 15_04_2022

--- Projects/Qui_veut_gagner_des_millions/view/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="footer">
-        <p>Created by Anthony.D (WebDeveloper Student | 2021-2022 IEPSCF )
-        </p>
-    </div>
-</footer>
-</body>
-</html>
--- Projects/Qui_veut_gagner_des_millions/view/main.php
+++
@@ -1,7 +0,0 @@
-<main role="main" class="container-fluid">
-    <?php
-        
-        require_once __DIR__ . '/../content.php';
-        
-    ?>
-</main>
