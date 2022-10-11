043681735d7e5a662b34922fa226b4071222af56
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-10-11 21:17:26 +0200

    update 11_10_2022

---
+++ Projects/Pixel_Cars/scripts/script.js
@@ -0,0 +1,12 @@
+document.getElementById('car1').style.top=465 + "px";
+document.getElementById('car2').style.top=465 + "px";
+document.getElementById('car3').style.top=490 + "px";
+document.getElementById('car4').style.top=525 + "px";
+document.getElementById('bus').style.top=410 + "px";
+
+document.getElementById('car1').style.left=1400 + "px";
+document.getElementById('car2').style.left=1480 + "px";
+document.getElementById('car3').style.left=300 + "px";
+document.getElementById('car4').style.left=400 + "px";
+document.getElementById('bus').style.left=1500 + "px";
+
